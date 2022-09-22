6ad0d79266c68ec0e0c3e4ca66c95d73c6cd1e4f
Muhammed Shijas <shijasck11@gmail.com>
2022-09-22 16:15:12 -0400

    Testing for Netlify: route fix 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 const routerOptions: ExtraOptions = {
   scrollPositionRestoration: 'enabled',
   anchorScrolling: 'enabled',
   scrollOffset: [0, 64],
 };
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, routerOptions)],
