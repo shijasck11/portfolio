03343cd784b80df9d2b2dac2704478ccb7966fe6
Muhammed Shijas <shijasck11@gmail.com>
2022-09-29 15:12:14 -0400

    Testing hosting with changes in package.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/shijasPorfolio'));
+app.use(express.static(__dirname , 'dist','shijasPorfolio'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/shijasPorfolio/index.html'));});
+'dist','shijasPorfolio','index.html'));});
 app.listen(process.env.PORT || 8080);
